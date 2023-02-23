88ec95021bdabe3623a979f85181276609c6bbd1
unknown <bhotnikhil25@gmail.com>
2023-02-23 10:41:38 +0530

    [Nikhil]-Fibonacci Series

---
+++ FibonacciSeries.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    internal class FibonacciSeries
+    {
+        public static void Fibonacci_Recurrsion(int len)
+        {
+            int a = 0, b = 1, c = 0;
+            Console.Write("{0} {1}", a, b);
+            for (int i = 2; i < len; i++)
+            {
+                c = a + b;
+                Console.Write(" {0}", c);
+                a = b;
+                b = c;
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace LogicalProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            FibonacciSeries.Fibonacci_Recurrsion(5);
+        }
+    }
+}
